d7a9955d04a21b1d400cbec6f3e060a8daf3befa
zh <2514385224@qq.com>
2020-04-14 20:36:31 +0800

    Initial commit

---
+++ src/main/java/com/jju/zhouf/App.java
@@ -0,0 +1,13 @@
+package com.jju.zhouf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
